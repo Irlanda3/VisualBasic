fc04d9a71b030a8c037f96ae13985a4936acc00b
8A <32315516+Irlanda3@users.noreply.github.com>
2022-05-13 17:21:20 -0600

    Add files via upload

---
+++ errorDetection/errorDetection/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblCreditCard = New System.Windows.Forms.Label()
+        Me.txtCardNumber = New System.Windows.Forms.TextBox()
+        Me.btnValidateNumber = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtValid = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lblCreditCard
+        '
+        Me.lblCreditCard.AutoSize = True
+        Me.lblCreditCard.Location = New System.Drawing.Point(40, 31)
+        Me.lblCreditCard.Name = "lblCreditCard"
+        Me.lblCreditCard.Size = New System.Drawing.Size(102, 13)
+        Me.lblCreditCard.TabIndex = 0
+        Me.lblCreditCard.Text = "Credit Card Number:"
+        '
+        'txtCardNumber
+        '
+        Me.txtCardNumber.Location = New System.Drawing.Point(174, 31)
+        Me.txtCardNumber.Name = "txtCardNumber"
+        Me.txtCardNumber.Size = New System.Drawing.Size(187, 20)
+        Me.txtCardNumber.TabIndex = 1
+        '
+        'btnValidateNumber
+        '
+        Me.btnValidateNumber.Location = New System.Drawing.Point(174, 86)
+        Me.btnValidateNumber.Name = "btnValidateNumber"
+        Me.btnValidateNumber.Size = New System.Drawing.Size(187, 23)
+        Me.btnValidateNumber.TabIndex = 2
+        Me.btnValidateNumber.Text = "Validate Number"
+        Me.btnValidateNumber.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(140, 148)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(84, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Is number valid?"
+        '
+        'txtValid
+        '
+        Me.txtValid.Location = New System.Drawing.Point(269, 141)
+        Me.txtValid.Name = "txtValid"
+        Me.txtValid.Size = New System.Drawing.Size(92, 20)
+        Me.txtValid.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 209)
+        Me.Controls.Add(Me.txtValid)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.btnValidateNumber)
+        Me.Controls.Add(Me.txtCardNumber)
+        Me.Controls.Add(Me.lblCreditCard)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblCreditCard As Label
+    Friend WithEvents txtCardNumber As TextBox
+    Friend WithEvents btnValidateNumber As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtValid As TextBox
+End Class
---
+++ errorDetection/errorDetection/Form1.vb
@@ -0,0 +1,125 @@
+﻿Public Class Form1
+    Dim cardNumber As String
+    Dim cardIntegers As Integer
+    Dim i As Integer
+    Dim numeroMenosNueve, totalAllIntegers As Integer
+    Dim totalTemp As Integer
+
+
+    Private Sub btnValidateNumber_Click(sender As Object, e As EventArgs) Handles btnValidateNumber.Click
+        cardNumber = txtCardNumber.Text 'recibo un string de numeros y lo guardo como String
+
+
+        'txtValid.Text = cardNumber
+
+        For i = 0 To cardNumber.Length - 1 ' solo puedo iterar un string no un integer type
+
+            'Debug.Print(cardNumber(i))
+            Dim total, porDoble As Integer
+
+            txtValid.Text = cardNumber(i) 'Imprime los numeros pero en columna por eso solo me aparece el ultimo
+            MessageBox.Show(cardNumber.Length - 1)
+
+            'total += Val(cardNumber(i)) 'Este convierte char a integer y acumula
+            total = Val(cardNumber(i)) 'Este convierte char a integer y imprimime todos los caracteres en todos los indices
+            Debug.Print(total)
+            'AHORA NECESITO MULTIPLICAR CADA DIGITO  X2 
+            porDoble = total * 2
+            Debug.Print(porDoble)
+            i += 1 ' add 1 to i. In other words is next one plus one
+
+            Dim temp, temp2 As Integer
+
+            ' convertimos a string otra vez para por usar el .length
+            'temp = CStr(porDoble)
+            'im temp2 As String = temp(i)
+
+
+
+            'si porDoble tiene dos digitos. si es numero de dos dijitos entonces resta 9
+            If porDoble >= 10 Then
+
+                'resta  9 de el numero que tenga dos digitos
+
+                numeroMenosNueve = porDoble - 9
+                Debug.Print(numeroMenosNueve)
+
+                'numeroMenosNueve += CStr(numeroMenosNueve(i))
+                'temp = CStr(numeroMenosNueve) 'convertimos el resultado de los numeros destados menos 9 a string
+
+                ' 
+                temp += (numeroMenosNueve + numeroMenosNueve) - numeroMenosNueve
+                Debug.Print(temp)
+                MessageBox.Show(temp)
+            ElseIf porDoble < 10 Then
+                temp2 += porDoble
+                MessageBox.Show("este es si poDoble es < 10 entonces deberia darme 16")
+                MessageBox.Show(temp2)
+
+
+            End If
+
+            totalTemp = temp + temp2
+            MessageBox.Show("este nos tiene que dar 25:   " & totalTemp)
+            Debug.Print(totalTemp)
+
+            sumando()
+
+
+
+
+        Next
+
+
+
+    End Sub
+        
+    Sub sumando()
+        'llamamos function add
+        cardIntegers = add(cardNumber)
+        MessageBox.Show("ESTE TENDRIA QUE DAR 35:   " & cardIntegers)
+
+        totalAllIntegers = totalTemp + cardIntegers
+        MessageBox.Show(totalAllIntegers)
+
+        'if 60 mod 10 = 0 entonces es entero
+        If totalAllIntegers Mod 10 = 0 Then
+            'if it returns cero then it is an integer and a multiple of 10
+            ' if 60 mod 10 = 0 Then accept credit card
+            txtValid.Text = True
+
+        Else
+            txtValid.Text = False
+
+        End If
+    End Sub
+
+
+    Function add(s As String) As Integer
+
+        cardNumber = txtCardNumber.Text 'recibo un string de numeros y lo guardo como String
+        Dim total2 As Integer
+        Dim j As Integer ' tenemos que usar j pq si uso i me da un error
+        For j = 1 To cardNumber.Length - 1
+
+
+
+            'IMPLEMENTEMOS ALGO PARA SUMAR LOS DIGITOS REQUERIDOS
+            'total = CType(cardNumber(i), Integer) ' esta funcion es muy util para convertir excepto char a integer
+
+
+            'total += Val(cardNumber(i)) 'Este convierte char a integer y acumula
+            total2 += Val(cardNumber(j)) 'Este convierte char a integer y imprimime todos los caracteres en todos los indices
+            Debug.Print(total2)
+
+            j += 1 'el siguiente index mas uno
+
+        Next
+
+        Return total2
+
+    End Function
+
+
+
+End Class
